312a5cd536ef107e92e06625f3c70253878b1d94
ayush200144 <81906497+ayush200144@users.noreply.github.com>
2024-05-23 15:30:50 +0530

    Update counters.sol

--- counters.sol
+++ counters.sol
@@ -1,31 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
-import "https://github.com/ayush200144/counter/blob/main/counters.sol";
-import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
-import "github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Strings.sol";
-
-contract NFT is ERC721URIStorage {
-    using CustomCounters for CustomCounters.Counter;
-    CustomCounters.Counter private _tokenIds;
-    address public owner;
-
-    constructor() ERC721("Moon NFTS", "Moon") {
-        owner = msg.sender;
+library CustomCounters {
+    struct Counter {
+        uint256 _value; // default: 0
     }
 
-    function createToken() public returns (uint) {
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
-        string memory tokenURI = string(abi.encodePacked(
-            "https://firebasestorage.googleapis.com/v0/b/nfts-25c5b.appspot.com/o/metadata%2F", 
-            Strings.toString(newItemId), 
-            ".json?alt=media&token=54d63d19-cda5-40ba-8d69-1bcdb8042389"
-        ));
+    function current(Counter storage counter) internal view returns (uint256) {
+        return counter._value;
+    }
 
-        _mint(msg.sender, newItemId);
-        _setTokenURI(newItemId, tokenURI);
-        return newItemId;
+    function increment(Counter storage counter) internal {
+        unchecked {
+            counter._value += 1;
+        }
+    }
+
+    function decrement(Counter storage counter) internal {
+        uint256 value = counter._value;
+        require(value > 0, "Counter: decrement overflow");
+        unchecked {
+            counter._value = value - 1;
+        }
+    }
+
+    function reset(Counter storage counter) internal {
+        counter._value = 0;
     }
 }
