533dff7d561fbc1f48cd22df363c4eef09468551
ayush200144 <81906497+ayush200144@users.noreply.github.com>
2024-05-29 11:39:38 +0530

    Update newcounter.sol

--- newcounter.sol
+++ newcounter.sol
@@ -1,1 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Counter is Ownable {
+    uint256 private _count;
+
+    event CountedTo(uint256 number);
+
+    constructor() {
+        _count = 0;
+    }
+
+    function count() public onlyOwner {
+        _count += 1;
+        emit CountedTo(_count);
+    }
+
+    function getCount() public view returns (uint256) {
+        return _count;
+    }
+
+    function resetCount() public onlyOwner {
+        _count = 0;
+        emit CountedTo(_count);
+    }
+}
 
